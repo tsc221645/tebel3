579cea587deb7670d08d8b203d05a123313650b4
EscasanN <escalantesanchez3@gmail.com>
2024-08-14 22:16:14 -0600

    Renamed files in app/Model/

--- app/Models/Line.php
+++ app/Models/Line.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class orders_details extends Model
+class Line extends Model
 {
     use HasFactory;
 }
--- app/Models/Phone_number.php
+++ app/Models/Phone_number.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class lines extends Model
+class Phone_number extends Model
 {
     use HasFactory;
 }
