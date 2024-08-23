85ab96ab9056a43ba9cca14b2e09d8bcd38b7814
tsc221645 <tsc221645@uvg.edu.gt>
2024-08-23 00:15:26 +0000

    Seeder para pruebas db terminado

--- database/seeders/PruebasDBSeeder.php
+++ database/seeders/PruebasDBSeeder.php
@@ -13,6 +13,7 @@ class PruebasDBSeeder extends Seeder
      */
     public function run(): void
     {
+        /*
         // Insertar categorías
         DB::table('categories')->insert([
             ['name' => 'Automotriz'],
@@ -32,19 +33,56 @@ class PruebasDBSeeder extends Seeder
             ['name' => 'Gomy', 'line_description' => 'Productos de la linea Gomy'],
             ['name' => 'Limpio', 'line_description' => 'Productos de la linea Limpio'],
         ]);
+        */
 
-        /* Insertar productos
+        //Insertar productos
         DB::table('products')->insert([
             [
-                'name' => 'PASTA PARA LUSTRAR',
-                'url_products' => null, // Valor nulo para 'url_products'
-                'description' => 'Es una mezcla de ceras cien por ciento naturales que producen una capa protectora de alta duración resistente a rayones menores así como a la penetración y crecimiento de mohos en las superficies en que es aplicada. Recomendada para abrillantar pisos de cemento, madera, parquet, linolio, granito y otros.',
-                'price' => null, // Valor nulo para 'price'
-                'category_id' => 1,
-                'line_id' => 1, // Asociado con la primera línea insertada
+                'name' => 'AGUA DESTILADA',
+                'url_products' => "", // Valor nulo para 'url_products'
+                'description' => 'vida para la batería. Solución libre de iones (desionizada), para el buen funcionamiento de su batería. Para renovar el nivel del electrolito de baterías nuevas, cuando la pérdida es por evaporación.',
+                'price' => 20.89, // Valor nulo para 'price'
+                'category_id' => 9, //industrial
+                'line_id' => 3, //Auto
+                'popular' => false, // Valor predeterminado para 'popular'
+            ], 
+            [
+                'name' => 'Car Kool® 33%:',
+                'url_products' => "", // Valor nulo para 'url_products'
+                'description' => 'Prediluido Es es un anticongelante de vida extendida basado en etilenglicol para motores de alto desempeño. Con Tecnología anticorrosiva que incorpora aditivos de rápida acción, para la protección de radiadores de hierro y aluminio.  Car Kool® 33% es una mezcla perfecta con alto desempeño en todas las condiciones climáticas, protege de las temperaturas extremas por su alto punto de ebullición y disminuye la necesidad de cambiar el refrigerante anualmente.  •	“Vida útil por 3 años”.    •	Especialmente para carros nuevos y de última generación.',
+                'price' => 105.9, // 
+                'category_id' => 7, //Automotriz
+                'line_id' => 5, // Prodin
                 'popular' => false, // Valor predeterminado para 'popular'
             ],
+            [
+                'name' => 'Car Kool® 50%',
+                'url_products' => "", // Valor nulo para 'url_products'
+                'description' => 'Es una fórmula concentrada que proporciona una máxima protección anticorrosiva y lubricante. Para motores de gasolina y diesel con radiadores de todo tipo de metal como hierro, aluminio, latón y/o acero. Car Kool® 50% cuenta con propiedades que proporcionan un sistema de refrigeración contra climas cálidos y fríos. Puede utilizarse en todas las marcas de vehículos, livianos y  de trabajo pesado.   •	Provee protección extendida por 5 años o 150,000 millas (341,000 km.) •	Listo para usarse, sin diluirlo con agua. •	Temperatura de congelamiento -37ºC y temperatura de ebullición + 173 ºC. •	Disminuye los costos de mantenimiento a largo plazo.',
+                'price' => 105.9, // Valor nulo para 'price'
+                'category_id' => 7, //automotriz
+                'line_id' => 5, //Prodin
+                'popular' => false, // Valor predeterminado para 'popular'
+            ], 
+            [
+                'name' => 'LIMPIA INYECTORES - DIESEL TREATMENT',
+                'url_products' => "", // Valor nulo para 'url_products'
+                'description' => 'Lubrica partes vitales, protección contra el óxido y la corrosión, reduce el humo negro, extiende la vida de los inyectores de combustible, mantiene más limpios los inyectores, mejora el flujo de combustible, etc.',
+                'price' => 19.52, // Valor nulo para 'price'
+                'category_id' => 7, //automotriz
+                'line_id' => 3, // Auto
+                'popular' => false, // Valor predeterminado para 'popular'
+            ], 
+            [
+                'name' => 'ALCO GEL',
+                'url_products' => "", // Valor nulo para 'url_products'
+                'description' => 'Es un antibacterial para desinfectar manos y superficies. Está elaborado con alcohol al 70% por lo que mata el 99.9 % de virus y bacterias.',
+                'price' => 130.50, // Valor nulo para 'price'
+                'category_id' => 11, //limpieza
+                'line_id' => 9, // limpio
+                'popular' => false, // Valor predeterminado para 'popular'
+            ], 
         ]);
-        */
+        
     }
 }
