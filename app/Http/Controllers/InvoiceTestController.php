052cea9ee496b586dd7f8274998968aa5104b309
tsc221645 <tsc221645@uvg.edu.gt>
2024-09-27 17:18:13 +0000

    modificacion template invoices

--- app/Http/Controllers/InvoiceTestController.php
+++ app/Http/Controllers/InvoiceTestController.php
@@ -11,6 +11,7 @@ class InvoiceTestController extends Controller
 {
     public function generateInvoice()
     {
+        // Información del cliente
         $customer = new Buyer([
             'name'          => 'Ermenejildo',
             'custom_fields' => [
@@ -18,15 +19,23 @@ class InvoiceTestController extends Controller
             ],
         ]);
 
+        // Creación de un ítem
         $item = InvoiceItem::make('Gomy 8oz')->pricePerUnit(20);
 
+        // Creación de la factura
         $invoice = Invoice::make()
             ->buyer($customer)
-            ->discountByPercent(10)
-            ->taxRate(27)
-            ->shipping(35)
-            ->addItem($item);
+            ->discountByPercent(10)  // Descuento
+            ->taxRate(27)  // Impuesto
+            ->shipping(35)  // Envío
+            ->currencySymbol('Q')  // Establecer el símbolo de quetzal
+            ->currencyCode('GTQ')  // Establecer el código de la moneda
+            ->currencyFormat('{SYMBOL}{VALUE}')  // Formato de moneda
+            ->currencyThousandsSeparator(',')  // Separador de miles
+            ->currencyDecimalPoint('.')  // Punto decimal
+            ->addItem($item);  // Agregar el ítem
 
+        // Generar y devolver la factura en el navegador
         return $invoice->stream();
     }
 }
